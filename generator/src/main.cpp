f036ec4a88a374928217c5d858d627a6df5bf3b0
Michael Kuc <michaelkuc6@gmail.com>
2020-07-25 21:06:12 +0100

    Check if specification file was able to be read.
    Don't begin generating the framework files if the file has not been
    successfully opened.

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,8 +1,9 @@
-#include "driver.hpp"
-#include "templates.hpp"
 #include <fstream>
 #include <iostream>
 #include <mstch/mstch.hpp>
+
+#include "driver.hpp"
+#include "templates.hpp"
 
 std::string symbolNameToOutputFile(const std::string &symbolName);
 
@@ -14,13 +15,21 @@ int main(int argc, char *argv[]) {
 
   Grammar::Driver driver{};
   std::ifstream specFile{argv[1]};
+
+  if (!specFile) {
+    std::cerr
+	<< "Failed to open file '" << argv[1]
+	<< "' for writing.\n Make sure this file exists and is readable.\n";
+    return 2;
+  }
+
   driver.parse(specFile);
   const auto context = driver.getContext();
 
   for (const auto &templateFile : templateFiles) {
     std::string strTemplate{templateFile.contents};
     std::ofstream outputFile{driver.getSafeName() +
-                             symbolNameToOutputFile(templateFile.symbolName)};
+			     symbolNameToOutputFile(templateFile.symbolName)};
 
     outputFile << mstch::render(strTemplate, context);
     outputFile.close();
@@ -32,11 +41,10 @@ int main(int argc, char *argv[]) {
 std::string symbolNameToOutputFile(const std::string &symbolName) {
   std::string outputFileName(symbolName.size(), static_cast<char>(NULL));
   std::transform(symbolName.begin(), symbolName.end(), outputFileName.begin(),
-                 [](char letter) {
-                   if (letter == '_')
-                     return '.';
-                   return letter;
-                 });
+		 [](char letter) {
+		   if (letter == '_') return '.';
+		   return letter;
+		 });
 
   return outputFileName;
 }
